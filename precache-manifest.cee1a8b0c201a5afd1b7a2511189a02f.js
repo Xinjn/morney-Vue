12568911da0115bacfd4e85f9d5e3a06ea2fd790
Xinjn <422075616@qq.com>
2021-08-09 04:35:08 +0800

    init

---
+++ precache-manifest.cee1a8b0c201a5afd1b7a2511189a02f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d28ea7db37b69f74511",
+    "url": "/morney-Vue/css/app.2af0fb13.css"
+  },
+  {
+    "revision": "5fdb6153748affcee4bd",
+    "url": "/morney-Vue/css/chunk-vendors.37b3d8f8.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/morney-Vue/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/morney-Vue/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "cf86dd8a8aead1fdd2269d82d82dde74",
+    "url": "/morney-Vue/index.html"
+  },
+  {
+    "revision": "2d28ea7db37b69f74511",
+    "url": "/morney-Vue/js/app.63218883.js"
+  },
+  {
+    "revision": "5fdb6153748affcee4bd",
+    "url": "/morney-Vue/js/chunk-vendors.18f814ef.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-Vue/manifest.json"
+  },
+  {
+    "revision": "e581e69c5532a8e552c5b2fd06446685",
+    "url": "/morney-Vue/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-Vue/robots.txt"
+  }
+]);
